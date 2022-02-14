82018a4486e634faf14a8ce2677dcfcafabfe838
Vadim Shabashov <nasekomysh@mail.ru>
2022-02-14 23:45:00 +0300

    Add HW02

--- expr/src/Combinators.hs
+++ expr/src/Combinators.hs
@@ -1,12 +1,14 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Combinators where
 
 newtype Parser a = Parser { runParser :: String -> Maybe (String, a) }
 
--- first [(sep, second), (sep', third)] -> sep' (sep (first, second)) third
 
 leftAssoc :: (elem -> sep -> elem -> elem) -> (elem, [(sep, elem)]) -> elem
 leftAssoc f (first, rest) =
   foldl (\acc (sep, elem) -> f acc sep elem) first rest
+
 
 rightAssoc :: (elem -> sep -> elem -> elem) -> (elem, [(sep, elem)]) -> elem
 rightAssoc f (first, rest) =
@@ -27,46 +29,49 @@ rightAssoc f (first, rest) =
 --       | ( Expr )
 
 -- Expr (op Expr) (op Expr) ... (op Expr) -> (Expr, [(op, Expr)])
-list :: Parser elem -> Parser sep -> Parser (elem, [(sep, elem)])
-list elem sep =
-    elem `andThen` \first ->
-    goParser `andThen` \rest ->
-    (Parser $ \str -> Just (str, (first, rest)))
-
-    -- do
-    --   first <- elem
-    --   rest <- goParser
-    --   return $ (first, rest)
-
-
-    -- Parser $ \str ->
-    --   case runParser elem str of
-    --     Just (str', first) ->
-    --       case go str' of
-    --         Just (str'', rest) -> Just (str'', (first, rest))
-    --         Nothing -> Just (str', (first, []))
-    --     Nothing -> Nothing
+list :: Parser expr -> Parser sep -> Parser (expr, [(sep, expr)])
+list expr sep = do
+  first <- expr
+  rest <- goParser <|> return [] -- т.к. может не быть продолжения
+  return (first, rest)
   where
-    goParser = Parser go
-    go str =
-      case runParser sep str of
-        Just (str', sep') ->
-          case runParser elem str' of
-            Just (str'', elem') ->
-              case go str'' of
-                Just (str''', res) ->
-                  Just (str''', (sep', elem') : res)
-                Nothing ->
-                  Just (str'', [(sep', elem')])
-            Nothing -> Nothing
-        Nothing -> Just (str, [])
+    goParser = do
+      sep' <- sep
+      expr' <- expr
+      res <- goParser <|> return [] -- т.к. продолжение имеет конец
+      return ((sep', expr') : res)
 
--- andThen == >>=
-andThen :: Parser a1 -> (a1 -> Parser a2) -> Parser a2
-andThen l r = Parser $ \str ->
-  case runParser l str of
-    Just (str', res) -> runParser (r res) str'
-    Nothing -> Nothing
+
+instance Functor Parser where
+  fmap f p = Parser $ \str ->
+    case runParser p str of
+      Just (s', res) -> Just (s', f res)
+      Nothing -> Nothing
+
+
+instance Applicative Parser where
+  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  p1 <*> p2 = Parser $ \str ->
+    case runParser p1 str of
+      Just (str', f) ->
+        case runParser p2 str' of
+          Just (str'', x) -> Just (str'', f x)
+          Nothing -> Nothing
+      Nothing -> Nothing
+
+  pure :: a -> Parser a
+  pure x = Parser $ \str -> Just (str, x)
+
+
+instance Monad Parser where
+  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  p >>= k = Parser $ \str ->
+    case runParser p str of
+      Just (str', res) -> runParser (k res) str'
+      Nothing -> Nothing
+
+  return :: a -> Parser a
+  return = pure
 
 
 char :: Char -> Parser Char
@@ -75,14 +80,9 @@ char c = Parser $ \str ->
     (h : t) | c == h -> Just (t, c)
     _ -> Nothing
 
+
 (<|>) :: Parser a -> Parser a -> Parser a
 l <|> r = Parser $ \str ->
   case runParser l str of
     Just (str', res) -> Just (str', res)
     Nothing -> runParser r str
-
-instance Functor Parser where
-  fmap f p = Parser $ \str ->
-    case runParser p str of
-      Just (s', res) -> Just (s', f res)
-      Nothing -> Nothing
