df12e25ef9c51de296af76c522d8c93d0edecada
berezhkoE <berezhko.ei@gmail.com>
2022-03-20 21:52:21 +0300

    Added arguments parsing

--- application/main.py
+++ application/main.py
@@ -3,18 +3,42 @@
 import lib.parser as parser
 import sys
 
+
 def main(args):
-    if(len(args) < 1):
+    if len(args) < 1:
         print("No file specified!")
         return
+
     try:
-        a = parser.parseFromFile(args[1])
+        automat = parser.parseFromFile(args[1])
     except parser.ParseError as e:
         print(e.message)
         return
-    #FIXME: test
-    print(a.go("aaaaabbbc"))
-        
+
+    while True:
+        arguments = sys.stdin.readline().split(maxsplit=1)
+        if len(arguments) == 1:
+            command = arguments[0]
+            if command == "exit":
+                return
+            elif command in ["check"]:
+                print("Not enough arguments")
+            else:
+                print(f"Command {command} not found")
+
+        elif len(arguments) > 1:
+            command, arguments = arguments
+            if command == "check":
+                if automat.go(arguments.strip()):
+                    print("String is accepted")
+                else:
+                    print("String is not accepted")
+            else:
+                print(f"Command {command} not found")
+
+        else:
+            print("Not enough arguments")
+
 
 if __name__ == "__main__":
     main(sys.argv)
